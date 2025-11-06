cdcdb4e5bc9344dff34168cfc540256f6c444a06
AniEsther <aniestheronyinye@gmail.com>
2025-11-06 12:05:22 +0100

    My first commit

--- basic_operations.py
+++ basic_operations.py
@@ -10,6 +10,6 @@ difference = number1 - number2
 product = number1 * number2
 
 # Print results
-print("Sum of", number1, "and", number2, "is", sum_result)
-print("Difference of", number1, "and", number2, "is", difference)
-print("Product of", number1, "and", number2, "is", product)
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Substraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
